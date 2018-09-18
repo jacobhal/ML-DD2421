1e52bf40b9ea8328b3458d6b351d4282fb85e2d8
Jacob <jackeaik@hotmail.com>
2018-09-18 14:03:09 +0200

    Lab 2 skeleton code

--- svm/main.py
+++ svm/main.py
@@ -1,3 +1,26 @@
-import numpy as np
-import scipy as sc
-import matplotlib as plt
+import numpy, random, math
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+
+# Number of training samples
+N = 100
+start = numpy.zeros(N)
+C = None
+B = [(0, C) for b in range(N)]
+
+def zerofun(vec):
+    scalar = 0
+    return scalar
+
+XC = constraint = {'type':'eq', 'fun':zerofun(B)}
+
+print(XC)
+
+def main():
+    ret = minimize(objective, start,
+    bounds = B, constraints = XC)
+    alpha = ret['x']
+
+def objective(vec):
+    scalar = 0
+    return scalar

aa7623002c5257c5df14b6c90c4e4df96d66368e
Jacob <jackeaik@hotmail.com>
2018-09-27 15:58:30 +0200

    Test class merge

--- svm/test.py
+++ svm/test.py
@@ -1,11 +1,9 @@
 import test_data as data
 
-<<<<<<< HEAD
 x = data.TestData(40, True)
 x.generate_data()
 
 print(f"Inputs: \n {x.inputs}")
 print(f"Targets: \n {x.targets}")
-=======
+
 print(data.targets)
->>>>>>> 9b287742545ae5a77596a820eb4e8efdc6748d00

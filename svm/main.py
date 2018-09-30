1351767bef1e10786f9c24b5cdb2195eaf658274
Jacob <jackeaik@hotmail.com>
2018-09-30 16:29:44 +0200

    Modified objective function and support vectors seems to be correct now

--- svm/main.py
+++ svm/main.py
@@ -27,42 +27,68 @@ for i in range(N):
     for j in range(N):
         preComputedMatrix[i][j] = data.targets[i] * data.targets[j] * kernel_function(data.inputs[i], data.inputs[j])
 
+def printInputData():
+    print("Input data")
+    for p in data.classA:
+        print(f"({p[0]}, {p[1]})")
+    for p in data.classB:
+        print(f"({p[0]}, {p[1]})")
+    print("")
+
 def zerofun(vec):
     scalar = numpy.dot(vec, data.targets)
-    sumScalar = numpy.sum(scalar)
-    return sumScalar
+    return scalar
 
 XC = constraint = {'type':'eq', 'fun':zerofun}
 
 # Take the alpha vector and return a scalar value by implementing the expression that should be minimized.
 def objective(alpha_vector):
-    alpha_dot = numpy.dot(alpha_vector, alpha_vector)
     alpha_sum = numpy.sum(alpha_vector)
 
-    completeMatrix = numpy.dot(alpha_dot, preComputedMatrix)
-    completeMatrixSum = numpy.sum(completeMatrix)
-    return 0.5 * completeMatrixSum - alpha_sum
+    matmul = numpy.dot(alpha_vector, preComputedMatrix)
+    vecmul = numpy.dot(alpha_vector, matmul)
+    return 0.5 * vecmul - alpha_sum
 
 def indicator(support_vector, target_value):
     # Calculate b value
-    b = numpy.sum(numpy.dot(nonZeroAlpha, target_values) * [kernel_function(support_vector, x) for x in support_vectors]) - target_value
-    val = numpy.sum(numpy.dot(nonZeroAlpha, target_values) * [kernel_function(support_vector, x) for x in support_vectors]) - b
+    tmp = [kernel_function(support_vector, x) for x in support_vectors]
+    b = numpy.dot(nonZeroAlpha, target_values) * numpy.sum(tmp) - target_value
+    val = numpy.dot(nonZeroAlpha, target_values) * numpy.sum(tmp) - b
     return val
 
-def plot():
+def plot(support_vectors, target_values):
+    # Plot input data
     plt.plot([p[0] for p in data.classA], [p[1] for p in data.classA], 'b. ')
     plt.plot([p[0] for p in data.classB], [p[1] for p in data.classB], 'r. ')
     plt.axis('equal') # Force same scale on both axes
     plt.savefig('svmplot.pdf') # Save a copy in a file
+
+
+    # Plot support vector points in green
+    for i, point in enumerate(support_vectors):
+        ind = indicator(point, target_values[i])
+        plt.plot(point[0], point[1], 'g.')
+
     plt.show() # Show the plot on the screen
 
+def plotDecisionBoundary():
+    # Plot decision boundary
+    xgrid = numpy.linspace(-5, 5)
+    ygrid = numpy.linspace(-4, 4)
+
+    TARGET = 1
+    grid = numpy.array([[indicator([x,y], TARGET) for x in xgrid] for y in ygrid])
+    plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0), colors=('red', 'black', 'blue'), linewidths=(1,3,1))
+    plt.show()
+
 def main():
+    printInputData()
     global support_vectors
     ret = minimize(objective, start, bounds = B, constraints = XC)
     success = ret['success']
     alpha = ret['x']
     if success:
-        print("Found a solution\n", alpha)
+        print("Alpha vector\n", alpha)
         # Find all alpha values above a certain threshhold and get the
         # corresponding inputs and target values
         nonZeroAlpha = alpha[alpha > 10 ** -5]
@@ -70,12 +96,16 @@ def main():
         sWithCorrValues = [(alpha[x], data.inputs[x], data.targets[x]) for x in indices[0]]
         # Unzip to get our target values in a list
         _, support_vectors, target_values = zip(*sWithCorrValues)
-        # Indicator function
-        ind = indicator(support_vectors[0], target_values[0])
-        print("Indicator:",ind)
 
+        print("\nSupport vectors")
+        # Indicator function
+        for i, point in enumerate(support_vectors):
+            ind = indicator(point, target_values[i])
+            print(f"({point[0]}, {point[1]}) classified as {ind}")
+
+        plot(support_vectors, target_values)
+        #plotDecisionBoundary()
     else:
         print("No solution found")
 
 main()
-#plot()
